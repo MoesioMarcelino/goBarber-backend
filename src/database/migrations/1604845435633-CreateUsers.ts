96213345746404bc60263618c00bb9b1a8fe3ff5
MoesioMarcelino <moesiomarcelino1@gmail.com>
2020-11-08 21:32:01 -0300

    Resolvendo migrations erradas

--- src/database/migrations/1604845435633-CreateUsers.ts
+++ src/database/migrations/1604845435633-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1604845435633 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/database/migrations/1604848544946-ChangeProviderToProviderIDInAppointments.ts
+++ src/database/migrations/1604848544946-ChangeProviderToProviderIDInAppointments.ts
@@ -10,32 +10,6 @@ export default class ChangeProviderToProviderIDInAppointments1604848544946
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.dropColumn('appointments', 'provider');
 
-    // Troca o tipo da coluna id (da tabela appointments) de varchar -> uuid
-    await queryRunner.changeColumn(
-      'appointments',
-      'id',
-      new TableColumn({
-        name: 'id',
-        type: 'uuid',
-        isPrimary: true,
-        generationStrategy: 'uuid',
-        default: 'uuid_generate_v4()',
-      }),
-    );
-
-    // Troca o tipo da coluna id (da tabela users) de varchar -> uuid
-    await queryRunner.changeColumn(
-      'users',
-      'id',
-      new TableColumn({
-        name: 'id',
-        type: 'uuid',
-        isPrimary: true,
-        generationStrategy: 'uuid',
-        default: 'uuid_generate_v4()',
-      }),
-    );
-
     await queryRunner.addColumn(
       'appointments',
       new TableColumn({
@@ -84,30 +58,6 @@ export default class ChangeProviderToProviderIDInAppointments1604848544946
     await queryRunner.dropForeignKey('appointments', 'AppointmentProvider');
 
     await queryRunner.dropColumn('appointments', 'provider_id');
-
-    await queryRunner.changeColumn(
-      'users',
-      'id',
-      new TableColumn({
-        name: 'id',
-        type: 'varchar',
-        isPrimary: true,
-        generationStrategy: 'uuid',
-        default: 'uuid_generate_v4()',
-      }),
-    );
-
-    await queryRunner.changeColumn(
-      'appointments',
-      'id',
-      new TableColumn({
-        name: 'id',
-        type: 'varchar',
-        isPrimary: true,
-        generationStrategy: 'uuid',
-        default: 'uuid_generate_v4()',
-      }),
-    );
 
     await queryRunner.addColumn(
       'appointments',
